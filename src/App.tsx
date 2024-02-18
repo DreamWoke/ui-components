f1e228ad61aa03d89c7bb2f9f2ed37edacf3c93b
wake <wake@3new.com>
2024-02-18 10:36:49 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+// import ExampleTabs from "src/components/example-components/TabsTypeAExample";
+// import ExampleCarouselTypeB from "src/components/example-components/CarouselTypeBExample";
+// import { ExampleTabsTypeB } from "src/components/example-components/TabsTypeBExample";
+// import { MultiStepForm } from "src/components/MultiStepForm";
+// import { FormExample } from "src/components/example-components/FormExample";
+// import { Button } from "src/components/Button";
+// import { ExampleSteps } from "src/components/example-components/StepsExample";
+// import FileUploadExample from "src/components/example-components/FileUploadExample";
+// import ExampleCarouselTypeA from "src/components/example-components/CarouselTypeAExample";
+// import ExampleTour from "src/components/example-components/TourExample";
+// import ExampleTimeline from "src/components/example-components/TimelineExample";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      {/* <ExampleTabs />
+      <ExampleTabsTypeB />
+      <MultiStepForm /> */}
+      {/* <FormExample /> */}
+      {/* <ExampleSteps /> */}
+      {/* <ExampleCarouselTypeA /> */}
+      {/* <ExampleCarouselTypeB /> */}
+      {/* <FileUploadExample /> */}
+      example
+      {/* <ExampleTour /> */}
+      {/* <ExampleTimeline /> */}
+      {/* <Button type="fill" onClick={() => console.log("hi")}>
+        Hello
+      </Button>
+      <Button onClick={() => console.log("hi")}>Hello</Button> */}
+    </>
+  );
+}
+
+export default App;
